7cc32b7d48d8dbd2cfa4cf9c56e344675390a26e
Alexey Chulochnikov <chulochnikov@gmail.com>
2014-09-23 00:06:31 +0300

    - add and edit functionality removed

--- ios8Cookbook/MasterViewController.swift
+++ ios8Cookbook/MasterViewController.swift
@@ -11,8 +11,7 @@ import UIKit
 class MasterViewController: UITableViewController {
 
 	var detailViewController: DetailViewController? = nil
-	var objects = NSMutableArray()
-
+	var objects = ["blurEffect", "test"]
 
 	override func awakeFromNib() {
 		super.awakeFromNib()
@@ -25,10 +24,7 @@ class MasterViewController: UITableViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
-		self.navigationItem.leftBarButtonItem = self.editButtonItem()
 
-		let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
-		self.navigationItem.rightBarButtonItem = addButton
 		if let split = self.splitViewController {
 		    let controllers = split.viewControllers
 		    self.detailViewController = controllers[controllers.count-1].topViewController as? DetailViewController
@@ -40,18 +36,12 @@ class MasterViewController: UITableViewController {
 		// Dispose of any resources that can be recreated.
 	}
 
-	func insertNewObject(sender: AnyObject) {
-		objects.insertObject(NSDate.date(), atIndex: 0)
-		let indexPath = NSIndexPath(forRow: 0, inSection: 0)
-		self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-	}
-
 	// MARK: - Segues
 
 	override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
 		if segue.identifier == "showDetail" {
 		    if let indexPath = self.tableView.indexPathForSelectedRow() {
-		        let object = objects[indexPath.row] as NSDate
+		        let object = objects[indexPath.row] as String
 		        let controller = (segue.destinationViewController as UINavigationController).topViewController as DetailViewController
 		        controller.detailItem = object
 		        controller.navigationItem.leftBarButtonItem = self.splitViewController?.displayModeButtonItem()
@@ -73,25 +63,15 @@ class MasterViewController: UITableViewController {
 	override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as UITableViewCell
 
-		let object = objects[indexPath.row] as NSDate
-		cell.textLabel?.text = object.description
+		let object = objects[indexPath.row] as String
+		cell.textLabel?.text = object
 		return cell
 	}
 
 	override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
 		// Return false if you do not want the specified item to be editable.
-		return true
+		return false
 	}
-
-	override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-		if editingStyle == .Delete {
-		    objects.removeObjectAtIndex(indexPath.row)
-		    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-		} else if editingStyle == .Insert {
-		    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-		}
-	}
-
 
 }
 
